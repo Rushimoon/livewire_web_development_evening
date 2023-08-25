4f3ea0346b5cd64e9cdddd61fa7b0cabf0dd651a
Rushimoon <rushikeshmoon1998@gmail.com>
2023-08-25 19:10:43 +0530

    functions

---
+++ day15_logicalOperaors/script.js
@@ -0,0 +1,58 @@
+//logical "and" "or" aur not(negation)
+
+console.log("--=-=-=-logiacl AND-=-=-=-")
+//and ->&&
+console.log(true &&true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+console.log("--=-=-=-logiacl OR-=-=-=-")
+//or->|| 
+console.log(true ||true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+
+// not->!
+console.log("--=-=-=-logiacl NOR-=-=-=-")
+
+
+console.log(!true)
+console.log(!false)
+
+
+
+//-=-=--======================================================================\\\
+//functions
+
+function  printkaro(){
+
+    console.log("this is our new function")
+    console.log("which is writen to perform specific task")
+    
+
+}
+
+
+
+printkaro();
+
+
+
+ function addkaro(a,b){
+
+  let sum=a+b;
+  return sum ;
+  console.log(addkaro(5,3))
+  console.log(addkaro(5,3))
+  console.log(addkaro(5,3))
+  console.log(addkaro(5,3))
+ }
+
+console.log(addkaro(5,3))
+ 
+let qqq= addkaro(4,5);
+console.log(qqq)
+
+
+//https://www.bewakoof.com/
